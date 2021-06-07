90c47829b6f61c30b71600309137fdd1a0b97a3e
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-06 18:05:49 -0700

    breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,19 @@
 const request = require('request');
 
-const breed = process.argv[2]
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
+const breed = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request(url, (error, response, body) => {
-const data = JSON.parse(body);
-console.log(breed);
-// console.log(body);
-// console.log(typeof body);
-console.log(data);
-console.log(typeof data);
-// console.log(response);
+  const data = JSON.parse(body);
+  console.log(breed);
+  // console.log(body);
+  // console.log(typeof body);
+  console.log(data);
+  console.log(typeof data);
+  // console.log(response);
 
-if (error) {
-  return callback(error)
-  } else {
+  if (error) {
+    return callback(error);
   }
 });
 
